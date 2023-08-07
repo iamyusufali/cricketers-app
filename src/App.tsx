46a9b8582ee7e95b107bec78753328d3a9fd29f4
yusufali19 <yusufali9398@gmail.com>
2023-08-07 15:38:56 +0530

    wrap app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import { RouterProvider } from 'react-router-dom';
 import router from './routes';
+import { Box } from '@chakra-ui/react';
 
 const App = () => {
-	return <RouterProvider router={router} />;
+	return (
+		<Box h='100vh' bg='gray.800'>
+			<RouterProvider router={router} />
+		</Box>
+	);
 };
 
 export default App;
