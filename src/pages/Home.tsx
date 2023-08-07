ac6986abfd01f25cf991d7238f12e2b1b55ff057
yusufali19 <yusufali9398@gmail.com>
2023-08-07 15:36:53 +0530

    style home page, prettier config updates

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,11 +4,15 @@ import { Link } from 'react-router-dom';
 
 const HomePage = () => {
 	return (
-		<Box mx='auto' mt='5em' maxW='35em'>
-			<Heading mb={4}>Cricketers Arena</Heading>
-			<Text fontSize='xl'>Get to know the best of the bests from the India cricket team</Text>
+		<Box mx='auto' pt='5em' maxW='35em'>
+			<Heading color='white' mb={4}>
+				Cricketers Arena
+			</Heading>
+			<Text color='white' fontSize='xl'>
+				Get to know the best of the bests from the India cricket team
+			</Text>
 			<Link to='/players'>
-				<Button size='lg' colorScheme='blue' mt='24px'>
+				<Button size='lg' colorScheme='green' mt='24px'>
 					View players list
 				</Button>
 			</Link>
